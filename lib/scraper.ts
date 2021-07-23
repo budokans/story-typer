a43d35b12c533d68bfe0a5b59dbc1817090a4550
budokans <68995729+budokans@users.noreply.github.com>
2021-07-23 20:34:53 +1200

    Scrape 100 or posts published after timestamp

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
 
 export const API_ENDPOINT = "http://fiftywordstories.com/wp-json/wp/v2/posts";
+const PAGE_LIMIT = 10;
+const START_PAGE = 1;
+const PER_PAGE = 100;
 
-export const getHTML = async (url: string): Promise<void> => {
+const getUrlWithParams = (url: string, page: number) => {
+  const timestamp = "";
+  return timestamp
+    ? `${url}?after=${timestamp}`
+    : `${url}?page=${page}&per_page=${PER_PAGE}`;
+};
+
+const getPosts = async (url: string): Promise<any[] | undefined> => {
   try {
-    const { data: html } = await axios.get(url);
-    return html;
+    const { data } = await axios.get(url);
+    return data;
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
@@ -16,4 +26,10 @@ export const getHTML = async (url: string): Promise<void> => {
       console.log(`Error: ${error.message}`);
     }
   }
+};
+
+export const scrape = async (): Promise<any[] | undefined> => {
+  const url = getUrlWithParams(API_ENDPOINT, START_PAGE);
+  const scrapes = await getPosts(url);
+  return scrapes;
 };
--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getHTML, API_ENDPOINT } from "../../lib/scraper";
+import { scrape } from "../../lib/scraper";
 
 export default async function handler(
   _: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const html = await getHTML(API_ENDPOINT);
-  res.status(200).json(html);
+  const scrapes = await scrape();
+  const count = scrapes?.length;
+  res.status(200).json({ count, scrapes });
 }
