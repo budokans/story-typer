dcfa01e58a5854450f9d619c91068a6fa8b446da
Steven Webster <swebsterdev@gmail.com>
2021-07-26 21:42:20 +1200

    test: write better tests for prune()

--- __tests__/lib/formatScrapes.spec.ts
+++ __tests__/lib/formatScrapes.spec.ts
@@ -18,29 +18,24 @@ describe("prune", () => {
     id: 9304,
     date: "2021-07-21T03:00:24",
     link: "http://fiftywordstories.com/wp-json/wp/v2/posts",
-    title: { rendered: "some title " },
+    title: { rendered: "some title" },
     content: { rendered: "some sample text" },
     anotherField: "some value",
   };
 
-  test("returns a new object with correct properties", () => {
-    const result = prune(scrape);
-    expect(Object.keys(result).length).toEqual(7);
-    expect(Object.keys(result)).toEqual([
-      "id",
-      "title",
-      "authorBio",
-      "content",
-      "url",
-      "dateScraped",
-      "datePublished",
-    ]);
-    expect(Object.keys(result.content)).toEqual(["html", "text"]);
-  });
-
-  test("does not mutate passed object", () => {
-    prune(scrape);
-    expect(Object.keys(scrape).length).toEqual(6);
-    expect(typeof scrape.id).toEqual("number");
+  test("takes a scrape and produces a pruned scrape with the correct properties and values", () => {
+    const pruned = prune(scrape);
+    expect(pruned.id).toEqual("9304");
+    expect(pruned.title).toEqual("some title");
+    expect(pruned.authorBio).toEqual("some sample text");
+    expect(pruned.content.html).toEqual("some sample text");
+    expect(pruned.content.text).toEqual("some sample text");
+    expect(pruned.url).toEqual(
+      "http://fiftywordstories.com/wp-json/wp/v2/posts"
+    );
+    const re =
+      /^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]{3}[A-Z]{1})$/;
+    expect(re.test(pruned.dateScraped)).toEqual(true);
+    expect(pruned.datePublished).toEqual("2021-07-21T03:00:24");
   });
 });
