db8df3c8a9eab8b4de203585d546b542db7fc758
Steven Webster <swebsterdev@gmail.com>
2021-08-11 21:47:15 +1200

    Set up fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import "@fontsource/courier-prime/700.css";
+import "@fontsource/montserrat";
+import { theme } from "../theme";
 import { AuthProvider } from "@/context/auth";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
