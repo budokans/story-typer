f79a7ca86da729974e69ca304bb6f9558ce00798
Steven Webster <swebsterdev@gmail.com>
2021-08-25 21:01:41 +1200

    Move game into own container

---
+++ containers/game.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "@chakra-ui/react";
+import { useGame } from "@/hooks/useGame";
+import { Game } from "@/components/Game";
+
+export const GameContainer = () => {
+  const [viewportIsWiderThan768] = useMediaQuery("(min-width: 769px)");
+  const [isLargeViewport, setIsLargeViewport] = useState(false);
+  const {
+    onInitCountdown,
+    countdown,
+    status,
+    inputValue,
+    onInputChange,
+    userError,
+  } = useGame();
+
+  useEffect(() => {
+    viewportIsWiderThan768
+      ? setIsLargeViewport(true)
+      : setIsLargeViewport(false);
+  }, [viewportIsWiderThan768]);
+
+  enum CountDown {
+    "Go!",
+    "Set",
+    "Ready",
+  }
+  return (
+    <Game>
+      {isLargeViewport && (
+        <Game.StoryHeader>EDDIE D MOORE: You Have Arrived</Game.StoryHeader>
+      )}
+
+      <Game.StoryText>
+        When the GPS said that I had arrived at my destination, I found myself
+        parked in front of an abandoned country church on a dead end, gravel
+        road. Most of the paint had long peeled away, and the graveyard beside
+        it was full. I wondered who buried the last member.
+      </Game.StoryText>
+
+      <Game.Pad>
+        <Game.Input
+          onInputClick={onInitCountdown}
+          onInputChange={onInputChange}
+          value={inputValue}
+          gameStatus={status}
+          error={userError}
+        />
+        {isLargeViewport && <Game.ErrorAlert />}
+        <Game.BtnSm type="restart" />
+        <Game.BtnSm type="new" />
+      </Game.Pad>
+      {status === "countdown" ? (
+        <Game.Countdown>{CountDown[countdown]}</Game.Countdown>
+      ) : (
+        <Game.StopWatch idle={status === "idle"} />
+      )}
+    </Game>
+  );
+};
