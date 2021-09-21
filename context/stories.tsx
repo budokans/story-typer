d2d6a04b9158ad56904bb4ea35585fc95f9b5503
Steven Webster <swebsterdev@gmail.com>
2021-09-22 01:14:16 +1200

    reset gameCount on user sign out

--- context/stories.tsx
+++ context/stories.tsx
@@ -5,9 +5,11 @@ import {
   useState,
   Dispatch,
   SetStateAction,
+  useEffect,
 } from "react";
 import { useProvideStories } from "@/hooks/useProvideStories";
 import { StoryWithId } from "interfaces";
+import { useUser } from "@/hooks/useUser";
 
 interface StoryContext {
   stories: StoryWithId[];
@@ -21,6 +23,14 @@ const storiesContext = createContext<StoryContext | null>(null);
 export const StoriesProvider: React.FC = ({ children }) => {
   const [gameCount, setGameCount] = useState(1);
   const { stories, isLoading } = useProvideStories(gameCount);
+  const { data: user } = useUser();
+
+  // Listen for no user (signed out) and reset gameCount
+  useEffect(() => {
+    if (!user) {
+      setGameCount(1);
+    }
+  }, [user]);
 
   return (
     <storiesContext.Provider
