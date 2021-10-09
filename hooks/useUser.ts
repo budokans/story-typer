779ab1c3839c449c65c4c51f9655b5585854c2ec
Steven Webster <swebsterdev@gmail.com>
2021-10-09 16:46:06 +1300

    Add return type to useUser()

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,8 +1,9 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryResult } from "react-query";
 import { useAuth } from "@/context/auth";
 import { getUser } from "@/lib/db";
+import { User } from "interfaces";
 
-export const useUser = () => {
+export const useUser = (): UseQueryResult<User> => {
   const { userId } = useAuth();
   // Query below has enabled set to true, so the query won't run until userId is truthy.
 
