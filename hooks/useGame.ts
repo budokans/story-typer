c284333fd3f6e2d299715eccb710e2fd354cbb4f
Steven Webster <swebsterdev@gmail.com>
2021-10-09 16:13:37 +1300

    Move reducer and initial state to own module

--- hooks/useGame.ts
+++ hooks/useGame.ts
@@ -1,110 +1,16 @@
 import { ChangeEvent, useEffect, useReducer } from "react";
 import { useMutation, useQueryClient } from "react-query";
+import { UseGame } from "./useGame.types";
+import { GameReducer, initialState } from "./reducers/GameReducer";
 import { useUser } from "@/hooks/useUser";
 import { useStories } from "@/context/stories";
 import { useCountdown } from "./useCountdown";
 import { useTimer } from "./useTimer";
-import { GameAction, GameState, UseGame } from "./useGame.types";
 import { PrevGame, StoryWithId, User } from "interfaces";
 import { createPostSkipUser, createPostWinUser } from "@/lib/manageUser";
 import { createPrevGame, updateUserDataOnWin } from "@/lib/db";
 
-const initialState: GameState = {
-  status: "pending",
-  userError: false,
-  userStoredInput: "",
-  userCurrentInput: "",
-  wpm: 0,
-};
-
 const TIME_LIMIT = 120;
-
-const GameReducer = (state: GameState, action: GameAction): GameState => {
-  switch (action.type) {
-    case "storiesLoading": {
-      return {
-        ...state,
-        status: "pending",
-      };
-    }
-    case "storiesLoaded": {
-      return {
-        ...state,
-        status: "idle",
-      };
-    }
-    case "startCountdown": {
-      return {
-        ...state,
-        status: "countdown",
-      };
-    }
-    case "countdownComplete": {
-      return {
-        ...state,
-        status: "inGame",
-      };
-    }
-    case "inputValueChange": {
-      return {
-        ...state,
-        userCurrentInput: action.inputValue,
-      };
-    }
-    case "errorFree": {
-      return {
-        ...state,
-        userError: false,
-      };
-    }
-    case "userTypingError": {
-      return {
-        ...state,
-        userError: true,
-      };
-    }
-    case "wordCompleted": {
-      return {
-        ...state,
-        userStoredInput: state.userStoredInput.concat(state.userCurrentInput),
-        userCurrentInput: "",
-      };
-    }
-    case "win": {
-      return {
-        ...state,
-        status: "complete",
-        userStoredInput: "",
-        wpm: action.wpm,
-      };
-    }
-    case "outOfTime": {
-      return {
-        ...state,
-        status: "outOfTime",
-      };
-    }
-    case "reset": {
-      return {
-        ...state,
-        userError: false,
-        userStoredInput: "",
-        userCurrentInput: "",
-        status: "idle",
-        wpm: 0,
-      };
-    }
-    case "next": {
-      return {
-        ...initialState,
-        status: "idle",
-      };
-    }
-    default: {
-      throw new Error(`Action of type ${action} not recognized.`);
-    }
-  }
-};
 
 export const useGame = (): UseGame => {
   const [state, dispatch] = useReducer(GameReducer, initialState);
