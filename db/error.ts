cb96eef1d7a148460ffb6232807112a68b483921
Steven Webster <swebsterdev@gmail.com>
2023-03-23 16:59:13 +0800

    Fix: catchError not handling NotFound

--- db/error.ts
+++ db/error.ts
@@ -48,5 +48,7 @@ export const throwError = (error: DBError): never => {
   throw error;
 };
 
-export const catchError = (error: FirestoreError): never =>
-  F.pipe(error, buildFirestoreError, throwError);
+export const catchError = (error: NotFound | FirestoreError): never =>
+  error instanceof NotFound
+    ? throwError(error)
+    : F.pipe(error, buildFirestoreError, throwError);
