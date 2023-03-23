961c5b9037f448056d3eee4791b0b1f050078f01
Steven Webster <swebsterdev@gmail.com>
2023-03-23 15:53:33 +0800

    Read service account data from environment var

--- db/firebase-admin.ts
+++ db/firebase-admin.ts
@@ -1,15 +1,13 @@
-import {
-  initializeApp,
-  cert,
-  ServiceAccount,
-  getApps,
-} from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firelord";
-import serviceAccount from "../serviceAccount.json";
 
 if (getApps().length === 0) {
   initializeApp({
-    credential: cert(serviceAccount as ServiceAccount),
+    credential: cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    }),
   });
 }
 
